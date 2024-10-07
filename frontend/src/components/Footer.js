f56b3e86b5cadcec2dd2c7a0264451b6b31cfe06
Son Nguyen <hoangson091104@gmail.com>
2024-10-06 20:10:03 -0400

    WIP: BE done & React FE in progress

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Box, Typography, IconButton, Link } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
-import LanguageIcon from '@mui/icons-material/Language'; // For website link
+import LanguageIcon from '@mui/icons-material/Language';
 
 const Footer = () => {
   return (
