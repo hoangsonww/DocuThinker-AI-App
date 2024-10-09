63d2123493adb11e6c727f0de2399c578b92def1
Son Nguyen <hoangson091104@gmail.com>
2024-10-09 18:40:52 -0400

    Final: Finished React FE & React-Native

--- frontend/src/components/GoogleAnalytics.js
+++ frontend/src/components/GoogleAnalytics.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 const GoogleAnalytics = () => {
   return (
       <Helmet>
-        {/* Google Tag (gtag.js) */}
+        {/* Google Tag (gtag.js) - DO NOT USE IN YOUR PROJECT */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-RZBP95EQ7L"></script>
         <script>
           {`
