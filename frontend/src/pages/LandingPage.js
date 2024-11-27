d748f03e51e0dbfe819100b437299c93d0620a13
Son Nguyen <hoangson091104@gmail.com>
2024-11-27 18:00:43 -0500

    Final: Finished React FE & React-Native

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -13,14 +13,14 @@ import { Link } from "react-router-dom";
 import { keyframes } from "@emotion/react";
 
 const slideUp = keyframes`
-    from {
-        transform: translateY(30px);
-        opacity: 0;
-    }
-    to {
-        transform: translateY(0);
-        opacity: 1;
-    }
+  from {
+    transform: translateY(30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
 `;
 
 const LandingPage = ({ theme }) => {
